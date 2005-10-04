6ae58cf0ef0f14beea64551a4d1ced23528bbe6f
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2005-10-04 02:37:28 +0000

    configuration files are being renamed *.cfg

--- test/run_all_parser_t.sh
+++ test/run_all_parser_t.sh
@@ -5,7 +5,7 @@ DIR=../../../../test/`scramv1 arch`
 # Pass in name and status
 function die { echo Failed on $1 with status $2 ;  exit $2; }
 
-for name in *.txt
+for name in *.cfg
   do 
     ./parser_t.sh $name || die $name $?
   done
