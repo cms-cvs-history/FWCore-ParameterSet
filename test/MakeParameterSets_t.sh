e2635266d5bb7216531345e8816b220da0622063
Marc Paterno <sha1-50842b2973656903c5335439f1d47b932dc1ae12@cern.ch>
2006-01-17 22:59:37 +0000

    Adding protection against circular include and tests

--- test/MakeParameterSets_t.sh
+++ test/MakeParameterSets_t.sh
@@ -6,8 +6,13 @@ function die { echo Failed on $1 with status $2 ;  exit $2; }
 
 pushd ${LOCAL_TEST_DIR}
 
+# This call of the binary is expected to succeed
 ${LOCAL_TEST_BIN}/MakeParameterSets_t sample.txt || die sample.txt $?
-echo  MakeParameterSets_t.sh passed
+echo  MakeParameterSets_t sample.txt passed
+
+# This call of the binary is expected to fail.
+(${LOCAL_TEST_BIN}/MakeParameterSets_t circular.txt) && die circular.txt -1
+echo  MakeParameterSets_t circular.txt passed
 
 popd
 
