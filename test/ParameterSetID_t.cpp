7b9502a9dc28a1326221c9111c1ba8af4f3c4aff
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2006-02-13 22:08:17 +0000

    ParameterSetID moved from FWCore/EDProduct to DataFormats/Common

--- test/ParameterSetID_t.cpp
+++ test/ParameterSetID_t.cpp
@@ -3,14 +3,14 @@
 // This program tests the calculation of ParameterSetIDs for
 // ParameterSets.
 //
-// $Id: UntrackedParameters_t.cpp,v 1.2 2006/02/03 21:23:01 paterno Exp $
+// $Id: ParameterSetID_t.cpp,v 1.1 2006/02/06 20:40:19 paterno Exp $
 //----------------------------------------------------------------------
 #include <cassert>
 #include <iostream>
 #include <string>
 
 #include "FWCore/Utilities/interface/EDMException.h"
-#include "FWCore/EDProduct/interface/ParameterSetID.h"
+#include "DataFormats/Common/interface/ParameterSetID.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/MakeParameterSets.h"
 
