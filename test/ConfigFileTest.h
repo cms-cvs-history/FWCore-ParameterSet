5e6463b7ce6dfd5abd5e77d60ce5c74e5de15dbe
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-04-05 00:07:20 +0000

    Factored out some code for various file-reading tests

---
+++ test/ConfigFileTest.h
@@ -0,0 +1,44 @@
+#ifndef ParameterSet_ConfigFileTest_h
+#define ParameterSet_ConfigFileTest_h
+
+#include <cassert>
+#include <string>
+#include "FWCore/ParameterSet/interface/MakeParameterSets.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+#include <fstream>
+#include <iostream>
+
+/** framework to read in parameter sets from files, for testing
+ */
+
+class ConfigFileTest {
+public:
+  ConfigFileTest() {
+    instance_ = this;
+  }
+
+  int run(const std::string & filename) {
+    std::string filecontents;
+    assert ( edm::pset::read_whole_file(filename, filecontents) );
+
+    boost::shared_ptr<edm::ParameterSet> mainps;
+    boost::shared_ptr<std::vector<edm::ParameterSet> > services;
+
+    makeParameterSets(filecontents, mainps, services);
+    //edm::ParameterSet ps = 
+    //  mainps->getParameter<edm::ParameterSet>("@main_input");
+
+    return test(*mainps);
+  }
+
+  static ConfigFileTest * instance() {return instance_;}
+
+protected:
+  virtual int test(const edm::ParameterSet & p) = 0;
+  
+  static ConfigFileTest * instance_;
+
+};
+
+#endif
+
