a82a2f3ccec5b974c1d0a6588c048eb459602be8
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-04-05 00:08:19 +0000

    Factored out some code into ConfigTest and ConfigTestMain

--- test/MakeParameterSets_t.cpp
+++ test/MakeParameterSets_t.cpp
@@ -1,50 +1,27 @@
-#include <cassert>
+#include "FWCore/ParameterSet/test/ConfigFileTest.h"
+#include "FWCore/ParameterSet/test/ConfigTestMain.h"
 #include <iostream>
 
-#include "FWCore/ParameterSet/interface/MakeParameterSets.h"
-#include "FWCore/Utilities/interface/EDMException.h"
+class MakeParameterSetsTest : public ConfigFileTest {
+public:
+ virtual int test(const edm::ParameterSet & p)
+  {
+
+//    edm::ParameterSet ps =
+//      p.getParameter<edm::ParameterSet>("@main_input");
 
 
-#include <fstream>
-
-
-//------------------------------------------------------------
-// Program to test reading of configuration file to create
-// ParameterSet objects.
-//------------------------------------------------------------
-
-int work(int argc, char* argv[])
-{
-  // Because 'scramv1 build runtests' will run this test, and because
-  // we don't know how to pass this executable an argument, we make it
-  // pass immediately if no argument is supplied.
-  if ( argc == 1 ) return 0;
-
-  // Do the real testing.
-  // We require 1 argument: the name of the file we are to open.
-  if (argc != 2) return -1;
-
-  std::string filename(argv[1]);
-  std::string filecontents;
-  assert ( edm::pset::read_whole_file(filename, filecontents) );
-
-  boost::shared_ptr<edm::ParameterSet> mainps;
-  boost::shared_ptr<std::vector<edm::ParameterSet> > services;
-
-  makeParameterSets(filecontents, mainps, services);
-
-  edm::ParameterSet m1(mainps->getParameter<edm::ParameterSet>("m1"));
-  edm::ParameterSet m2(mainps->getParameter<edm::ParameterSet>("m2"));
+    edm::ParameterSet m1(p.getParameter<edm::ParameterSet>("m1"));
+    edm::ParameterSet m2(p.getParameter<edm::ParameterSet>("m2"));
   
-  assert( m1.getParameter<int>("a") == 1); // not from blob
-  assert( m2.getParameter<int>("a") == 2); // not from blob
+    assert( m1.getParameter<int>("a") == 1); // not from blob
+    assert( m2.getParameter<int>("a") == 2); // not from blob
 //   assert( m1.getParameter<double>("radius") == 0.5);  // from blob
 //   assert( m2.getParameter<double>("radius") == 0.75); // override blob
-  assert( m1.getParameter<double>("threshold") == 1.0); // from blob
-  assert( m2.getParameter<double>("threshold") == 1.0); // from blob
+    assert( m1.getParameter<double>("threshold") == 1.0); // from blob
+    assert( m2.getParameter<double>("threshold") == 1.0); // from blob
 
-  {
-    edm::ParameterSet ps = mainps->getParameter<edm::ParameterSet>("silly");
+    edm::ParameterSet ps = p.getParameter<edm::ParameterSet>("silly");
     std::vector<std::string> empty;
     std::vector<std::string> vs;
 
@@ -57,27 +34,12 @@ int work(int argc, char* argv[])
 
     vs = ps.getUntrackedParameter<std::vector<std::string> >("not_there", empty);
     assert ( vs.size() == 0 );
+    return 0;
   }
+};
 
-  return 0;
-}
 
-int main(int argc, char* argv[])
-{
-  int rc = 0;
-  try 
-    {
-      rc = work(argc, argv);
-    }
-  catch ( edm::Exception const& x )
-    {
-      rc = 1;
-      std::cerr << "Exception:\n" << x << '\n';
-    }
-  catch (...)
-    {
-      rc = 2;
-      std::cerr << "Unknown exception\n";
-    }
-  return rc;
-}
+ConfigFileTest * ConfigFileTest::instance_ = 0;
+// instantiate, so singleton mechanism picks it up
+static MakeParameterSetsTest test;
+
