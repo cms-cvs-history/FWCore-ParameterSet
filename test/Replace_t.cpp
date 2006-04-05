1a932841144ca80ebe91445c63326084e018c8c1
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-04-05 00:08:56 +0000

    tests the functionality of the replace command

---
+++ test/Replace_t.cpp
@@ -0,0 +1,32 @@
+#include "FWCore/ParameterSet/test/ConfigFileTest.h"
+#include "FWCore/ParameterSet/test/ConfigTestMain.h"
+#include <iostream>
+
+class ReplaceTest : public ConfigFileTest {
+public:
+ virtual int test(const edm::ParameterSet & p) {
+   edm::ParameterSet rome =
+    p.getParameter<edm::ParameterSet>("rome");
+
+   assert(rome.getParameter<int>("date") == 100);
+   assert(rome.getParameter<std::string>("motto") == "Toga! Toga!");
+   assert(rome.getParameter<std::vector<std::string> >("victorVector")[1] == "Russell Crowe");
+
+   edm::ParameterSet legion = rome.getParameter<edm::ParameterSet>("legion");
+   assert(legion.getParameter<std::vector<int> >("cohorts").size() == 10);
+
+   edm::ParameterSet government= rome.getParameter<edm::ParameterSet>("government");
+   assert(government.getParameter<std::string>("leader") == "Emperor");
+
+   // I don't know how to test the source replacement
+
+   return 0;
+ }
+
+};
+
+ConfigFileTest * ConfigFileTest::instance_ = 0;
+// instantiate, so singleton mechanism picks it up
+static ReplaceTest test;
+
+
