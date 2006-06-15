ae3a74e043aed5e37702c779f75f12eb665a8baa
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-06-15 22:08:44 +0000

    Changed productTag to InputTag

--- test/makepset_t.cppunit.cc
+++ test/makepset_t.cppunit.cc
@@ -5,7 +5,7 @@
  *  Created by Chris Jones on 5/18/05.
  *  Changed by Viji Sundararajan on 11-Jul-05.
  *
- * $Id: makepset_t.cppunit.cc,v 1.27 2006/06/08 17:50:50 rpw Exp $
+ * $Id: makepset_t.cppunit.cc,v 1.29 2006/06/12 00:37:17 wmtan Exp $
  */
 
 #include <algorithm>
@@ -281,8 +281,8 @@ void testmakepset::typesTest()
      "string sb3='    '\n"
      "vstring vs={\"1\",\"2\",\"a\"}\n"
      "VPSet vps ={ {bool b3=false} }\n"
-     "ProductTag input = Label:Instance:Alias\n"
-     "ProductTag output = Label2::Alias2\n";
+     "InputTag input = Label:Instance\n"
+     "InputTag output = Instance:Alias2\n";
    
    boost::shared_ptr<edm::ParameterSet> test = edm::pset::makePSet(kTest);
    //std::cout << test->toString() << std::endl;
@@ -321,17 +321,16 @@ void testmakepset::typesTest()
    CPPUNIT_ASSERT(1 == vps.size());
    CPPUNIT_ASSERT(false == vps.front().getParameter<bool>("b3"));
    
-   // ProductTag
-   edm::ProductTag inputProduct = test->getParameter<edm::ProductTag>("input");
-   edm::ProductTag outputProduct = test->getParameter<edm::ProductTag>("output");
+   // InputTag
+   edm::InputTag inputProduct = test->getParameter<edm::InputTag>("input");
+   //edm::OutputTag outputProduct = test->getParameter<edm::OutputTag>("output");
 
    CPPUNIT_ASSERT("Label"    == inputProduct.label());
    CPPUNIT_ASSERT("Instance" == inputProduct.instance());
-   CPPUNIT_ASSERT("Alias"    == inputProduct.alias());
 
-   CPPUNIT_ASSERT("Label2" == outputProduct.label());
-   CPPUNIT_ASSERT(""       == outputProduct.instance());
-   CPPUNIT_ASSERT("Alias2" == outputProduct.alias());
+   //CPPUNIT_ASSERT("Label2" == outputProduct.label());
+   //CPPUNIT_ASSERT(""       == outputProduct.instance());
+   //CPPUNIT_ASSERT("Alias2" == outputProduct.alias());
    //BOOST_CHECK_THROW(makePSet(*nodeList), std::runtime_error);
 }
 
