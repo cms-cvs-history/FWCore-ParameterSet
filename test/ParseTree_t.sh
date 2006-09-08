91921ed6d2912a6dfa82c0e40a164e1a1ad4f93a
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-09-08 02:08:52 +0000

    script to run Parse_t and Replace_t

---
+++ test/ParseTree_t.sh
@@ -0,0 +1,18 @@
+#!/bin/sh 
+
+
+# Pass in name and status
+function die { echo Failed on $1 with status $2 ;  exit $2; }
+
+pushd ${LOCAL_TEST_DIR}
+
+# This call of the binary is expected to succeed
+${LOCAL_TEST_BIN}/ParseTree_t Replace.cfg || die Replace.cfg $?
+echo  ParseTree_t Replace.cfg passed
+
+# This call of the binary is expected to fail.
+(${LOCAL_TEST_BIN}/Replace_t Replace.cfg) || die Replace.cfg $?
+echo  Replace_t Replace.cfg passed
+
+popd
+
