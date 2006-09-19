6376dfaf4709dc3a907858c83b0727f4845d8dd7
cmsbuild <>
2006-09-19 19:11:38 +0000

    This commit was manufactured by cvs2git to create tag 'FWCore-ParameterSet- CMSSW_1_0_1'.
    Sprout from master 2006-09-19 19:11:37 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'temporary dispensation to allow dots in names of PSets only, to ensure compatibility for MessageLogger users'
    Cherrypick from master 2006-07-06 18:47:53 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Changes/Additions to Provenance for release CMSSW_0_8_0':
        interface/Registry.h
        src/Registry.cc
        test/Registry_t.cpp

--- src/Registry.cc
+++ src/Registry.cc
@@ -1,5 +1,5 @@
 // ----------------------------------------------------------------------
-// $Id: Registry.cc,v 1.8 2006/09/06 19:16:29 paterno Exp $
+// $Id: Registry.cc,v 1.6.2.2 2006/06/29 21:55:17 paterno Exp $
 //
 // ----------------------------------------------------------------------
 
@@ -26,13 +26,6 @@ namespace edm
       std::vector<ParameterSet>::const_iterator i = all_main_psets.begin();
       std::vector<ParameterSet>::const_iterator e = all_main_psets.end();
       for (; i != e; ++i) reg->insertMapped(*i);
-      reg->extra().setID(main.id());
-    }
-
-    edm::ParameterSetID
-    getProcessParameterSetID(Registry const* reg)
-    {
-      return reg->extra().id();
     }
 
     void fill(Registry* reg, regmap_type& fillme)
