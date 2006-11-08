5cef39512da13460c5b045351ba5fb968c351740
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2006-11-08 00:40:46 +0000

    Test repeated parameter names, which are now illegal, thanks to the validate() method in CompositeNode

--- test/RepeatedNames_t.cpp
+++ test/RepeatedNames_t.cpp
@@ -1,7 +1,7 @@
 //----------------------------------------------------------------------
 //
 // This program tests the behavior repeated names in configuration files.
-// $Id: UntrackedParameters_t.cpp,v 1.6 2006/03/10 22:37:00 paterno Exp $
+// $Id: RepeatedNames_t.cpp,v 1.1 2006/06/21 21:16:30 paterno Exp $
 //----------------------------------------------------------------------
 #include <cassert>
 #include <iostream>
@@ -72,6 +72,38 @@ void testPathNames()
     }
 }
 
+
+void testParameterNames()
+{
+  std::string filetext =
+    "process TEST =\n"
+    "{\n"
+    "module a = Thing {\n"
+    "  int a = 1\n"
+    "  int a = 2\n"
+    "}\n"
+    "module b = Thing {}\n"
+    "path p1 = {  }\n"
+    "path p1 = {  }\n"
+    "}\n";
+  boost::shared_ptr<ParameterSet> mainps;
+  boost::shared_ptr<VPSet > services;
+  try
+    {
+      edm::makeParameterSets(filetext, mainps, services);
+      assert( "Failed to throw required exception" == 0 );
+    }
+  catch ( edm::Exception const& x )
+    {
+      // this is expected
+    }
+  catch ( ... )
+    {
+      assert( "Threw the wrong kind of exception\n" );
+    }
+}
+
+
 int main()
 {
   int rc = 1;
@@ -79,6 +111,7 @@ int main()
     {
       testModuleNames();
       testPathNames();
+      testParameterNames();
       rc = 0;
     }
   catch ( edm::Exception& x)
