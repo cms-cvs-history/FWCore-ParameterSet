a775bb1da5afaa6709ce797fcc5a47338b06c53a
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-03-22 06:29:06 +0000

    remove symbolic link after test adds it

--- test/ParseTree_t.sh
+++ test/ParseTree_t.sh
@@ -17,6 +17,7 @@ echo  Replace_t Replace.cfg passed
 # not sure if this is the right way
 ln -s ../../../../lib/*/libFWCoreParameterSet.so .
 python PythonParseTree_t.py || die PythonParseTree_t.py $?
+rm -f libFWCoreParameterSet.so
 
 popd
 
