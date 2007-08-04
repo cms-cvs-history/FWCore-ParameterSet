e112826c5537bb411b8363f8a801d796cd527dc7
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2007-08-04 00:19:32 +0000

    Include boost_python for python_parser

--- test/pythonparser_t.cpp
+++ test/pythonparser_t.cpp
@@ -2,8 +2,7 @@
 #include <string>
 
 #include "FWCore/Utilities/interface/EDMException.h"
-#include "FWCore/ParameterSet/src/PythonModule.h"
-#include "FWCore/ParameterSet/interface/PythonParameterSet.h"
+#include "FWCore/ParameterSet/interface/PythonProcessDesc.h"
 #include <boost/python.hpp>
 
 //------------------------------------------------------------
@@ -21,49 +20,8 @@ int work(int argc, char* argv[])
   // pass immediately if no argument is supplied.
   if ( argc == 1 ) return 0;
 
-  // If given an argument, that argument must be the name of a file to read.
-  Py_Initialize();
-  PyImport_AppendInittab( "libFWCoreParameterSet", &initlibFWCoreParameterSet );
-
-
-  object main_module((
-    handle<>(borrowed(PyImport_AddModule("__main__")))));
-
-  object main_namespace = main_module.attr("__dict__");
-
-  // load the library
-  object libModule( (handle<>(PyImport_ImportModule("libFWCoreParameterSet"))) );
-  // put it in the main namespace
-  main_namespace["libFWCoreParameterSet"] = libModule;
-
-  // make an instance in python-land
-  PythonParameterSet pythonParameterSet;
-  scope(libModule).attr("pythonParameterSet") = ptr(&pythonParameterSet);
-
-  try {
-      std::string initCommand("import FWCore.ParameterSet.Config as cms\n"
-                          "fileDict = dict()\n"
-                          "execfile('");
-      std::string fileName(argv[1]);
-      initCommand += fileName + "',fileDict)";
-
-
-      handle<>(PyRun_String(initCommand.c_str(),
-                            Py_file_input,
-                            main_namespace.ptr(),
-                            main_namespace.ptr()));
-      std::string command("cms.findProcess(fileDict).dumpConfig()");
-      handle<>(PyRun_String(command.c_str(),
-                            Py_eval_input,
-                            main_namespace.ptr(),
-                            main_namespace.ptr()));
-
-  }catch(...) {
-    Py_Finalize();
-    throw;
-  }
-  Py_Finalize();
-
+  PythonProcessDesc processDesc(argv[1]);
+  std::cout << processDesc.dump() << std::endl;
   return 0;
 }
 
