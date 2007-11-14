147d671678aadad4654a4591c9983ec02aa8c509
cmsbuild <>
2007-11-14 19:07:51 +0000

    This commit was manufactured by cvs2git to create tag 'FWCore-ParameterSet- CMSSW_1_7_5'.
    Sprout from master 2007-10-31 23:55:46 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'Handle the case where the user says 'replace a.b = {using bl}''
    Cherrypick from master 2007-08-20 22:54:22 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Remove unnecessary #include':
        src/ParseTree.cc
        src/PythonProcessDesc.cc
        src/PythonWrapper.h
        src/pythonFileToConfigure.cc
        test/pythonparser_t.cpp
    Cherrypick from master 2007-11-14 19:07:48 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'Can delete dumpSequencePython, because the str() methods do the same thing':
        python/Config.py
        python/Mixins.py
        python/Modules.py
        python/SequenceTypes.py
    Delete:
        scripts/EdmConfigTree
        scripts/edmConfigTree
        test/FullChainWrapper.py

--- test/FullChainWrapper.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-cfgFile="Configuration/Examples/data/FullChainExample.cfg"
-#cfgFile = "tmp.cfg"
-#cfgFile = "CSCDigitizerTest.cfg"
-from FWCore.ParameterSet.Config import include
-process = include(cfgFile)
-
