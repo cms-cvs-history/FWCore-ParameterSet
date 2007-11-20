c08c952545421fb59c22e6cba3da01d2f7571190
cmsbuild <>
2007-11-20 03:07:28 +0000

    This commit was manufactured by cvs2git to create tag 'FWCore-ParameterSet- CMSSW_1_6_10'.
    Sprout from master 2007-08-26 10:33:53 UTC Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch> 'Back to working python parsing version'
    Cherrypick from master 2007-11-20 03:07:27 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'Change back the casting to double(float(value)) in addDouble.  Not sure why PyReleaseValidation failed before without it, but it seems OK now':
        interface/Entry.h
        python/Config.py
        python/Mixins.py
        python/Modules.py
        python/SequenceTypes.py
        python/Types.py
        python/cfg2py.py
        python/parseConfig.py
        test/chainIncludeModule2.cfi
        test/complete.py
    Cherrypick from master 2007-10-24 14:03:04 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'Better comment out the FullChain test, for automation.  I can turn them back on for personal use.':
        src/Entry.cc
        src/ParameterSet.cc
        test/pythonparser_t.cpp

--- python/cfg2py.py
+++ python/cfg2py.py
@@ -2,13 +2,10 @@ import FWCore.ParameterSet.Config as cms
 import FWCore.ParameterSet.parseConfig as cmsParse
 from sys import argv
 
-print "import FWCore.ParameterSet.Config as cms"
-
 fileInPath = argv[1]
 
 if fileInPath.endswith('cfg'):
-    print cmsParse.parseCfgFile(fileInPath).dumpPython()
+    cmsParse.dumpCfg(fileInPath)
 else:
-    #print cmsParse.parseCffFile(fileInPath).dumpPython()
-    print  cmsParse.dump(fileInPath)
+    print cmsParse.dumpCff(fileInPath)
 
